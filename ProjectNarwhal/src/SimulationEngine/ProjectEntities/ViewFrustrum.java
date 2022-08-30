d0de5380f85a01cb1904e4dbccd10af0a2e41c24
Dereferenced-Null <lplace9001@gmail.com>
2022-08-30 18:22:52 +1000

    Initial Cursor Tracking

--- ProjectNarwhal/src/SimulationEngine/ProjectEntities/ViewFrustrum.java
+++ ProjectNarwhal/src/SimulationEngine/ProjectEntities/ViewFrustrum.java
@@ -3,18 +3,32 @@ package SimulationEngine.ProjectEntities;
 import SimulationEngine.Tools.Keyboard;
 import org.joml.Vector3f;
 import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWCursorPosCallbackI;
+
+import java.nio.DoubleBuffer;
+
 
 public class ViewFrustrum {
-
     private Vector3f location = new Vector3f(0,10,10);
     private float pitch;
     private float yaw;
     private float roll;
+    private double x;
+    private double y;
+    private long window;
 
     //default constructor
     public ViewFrustrum(long window){
+        this.window = window;
         Keyboard keyboard = new Keyboard();
         GLFW.glfwSetKeyCallback(window, keyboard::invoke);
+        GLFW.glfwSetCursorPosCallback(window, new GLFWCursorPosCallbackI() {
+            @Override
+            public void invoke(long window, double xpos, double ypos) {
+                x = xpos;
+                y = ypos;
+            }
+        }::invoke);
     };
 
     public void move(){
@@ -36,6 +50,9 @@ public class ViewFrustrum {
         if(Keyboard.isKeyDown(GLFW.GLFW_KEY_SPACE)){
             location.y +=  0.6f;
         }
+        //        GLFW.glfwGetCursorPos(window, x, y);
+        System.out.println(x);
+        System.out.println(y);
     }
 
     //getters
