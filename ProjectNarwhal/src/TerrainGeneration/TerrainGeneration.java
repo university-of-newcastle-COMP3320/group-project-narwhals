972b9aa56bf01f0b657646eede9755606108f753
thomaslegge98 <72440840+thomaslegge98@users.noreply.github.com>
2022-09-12 11:58:49 +1000

    Implementation of OpenSimplex

--- ProjectNarwhal/src/TerrainGeneration/TerrainGeneration.java
+++ ProjectNarwhal/src/TerrainGeneration/TerrainGeneration.java
@@ -1,28 +1,30 @@
 package TerrainGeneration;
 
-import java.awt.image.BufferedImage;
+import java.awt.*;
 
 public class TerrainGeneration {
-	for (int y = 0; y < height; y++) {
-		for (int x = 0; x < width; x++) {      
-			double nx = x/width - 0.5, ny = y/height - 0.5;
-			value[y][x] = noise(nx, ny);
+	public static void main(String[] args){
+		TerrainGeneration tg = new TerrainGeneration();
+		tg.drawMap(tg.generateMap(808, 10, 10));
+	}
+
+	public double[][] generateMap(long seed, int width, int height) {
+		double[][] map = new double[width][height];
+		OpenSimplexNoise simplex = new OpenSimplexNoise(seed);
+
+		for(int x=0; x<width; x++){
+			for(int y=0; y<height; y++){
+				map[x][y] = simplex.eval(x, y);
+			}
+		}
+		return map;
+	}
+
+	public void drawMap(double[][] map) {
+		for(int x = 0; x < map.length; x++) {
+			for(int y = 0; y < map[0].length; y++) {
+				Color color = new Color((int) map[x][y], (int) map[x][y], (int) map[x][y]);
+			}
 		}
 	}
-	
-	elevation[y][x] = noise(nx, ny);
-	elevation[y][x] = noise(5.56 * nx, 5.56 * ny);
-	elevation[y][x] = noise(x / wavelength, y / wavelength);
-	elevation[y][x] =    1 * noise(1 * nx, 1 * ny) +  0.5 * noise(2 * nx, 2 * ny) + 0.25 * noise(4 * nx, 4 * ny);
-	
-	e  =    1 * noise(1 * nx, 1 * ny);
-	+  0.5 * noise(2 * nx, 2 * ny);
-	+ 0.25 * noise(4 * nx, 4 * ny);
-	elevation[y][x] = e / (1 + 0.5 + 0.25);
-	
-	e =    1 * noise(1 * nx, 1 * ny);
-	+  0.5 * noise(2 * nx, 2 * ny);
-	+ 0.25 * noise(4 * nx, 4 * ny);
-	e = e / (1 + 0.5 + 0.25);
-	elevation[y][x] = Math.pow(e, 10.00);
 }
