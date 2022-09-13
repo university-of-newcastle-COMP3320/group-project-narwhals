3d8e771ac4142f0cc556b2192b9b8b2bda131ae0
thomaslegge98 <72440840+thomaslegge98@users.noreply.github.com>
2022-09-13 15:12:13 +1000

    Updated Colour Function

--- ProjectNarwhal/src/TerrainGeneration/TerrainGeneration.java
+++ ProjectNarwhal/src/TerrainGeneration/TerrainGeneration.java
@@ -1,30 +1,35 @@
 package TerrainGeneration;
 
 import java.awt.*;
+import java.lang.Math;
 
 public class TerrainGeneration {
 	public static void main(String[] args){
 		TerrainGeneration tg = new TerrainGeneration();
-		tg.drawMap(tg.generateMap(808, 10, 10));
+		Color[][] colour = tg.drawMap(tg.generateMap(808, 10, 10));
+		System.out.println(colour[3][2]);
 	}
 
 	public double[][] generateMap(long seed, int width, int height) {
 		double[][] map = new double[width][height];
+
 		OpenSimplexNoise simplex = new OpenSimplexNoise(seed);
 
 		for(int x=0; x<width; x++){
 			for(int y=0; y<height; y++){
-				map[x][y] = simplex.eval(x, y);
+				map[x][y] = Math.abs(simplex.eval(x, y));
 			}
 		}
 		return map;
 	}
 
-	public void drawMap(double[][] map) {
+	public Color[][] drawMap(double[][] map) {
+		Color colour[][] = new Color[10][10];
 		for(int x = 0; x < map.length; x++) {
 			for(int y = 0; y < map[0].length; y++) {
-				Color color = new Color((int) map[x][y], (int) map[x][y], (int) map[x][y]);
+				colour[x][y] = new Color((float) map[x][y], (float) map[x][y], (float) map[x][y]);
 			}
 		}
+		return colour;
 	}
 }
