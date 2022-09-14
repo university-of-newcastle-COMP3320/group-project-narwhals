81e5ddf009fd17e6f31a689530c9e6860270c8e4
thomaslegge98 <72440840+thomaslegge98@users.noreply.github.com>
2022-09-14 10:46:08 +1000

    Update

--- ProjectNarwhal/src/TerrainGeneration/TerrainGeneration.java
+++ ProjectNarwhal/src/TerrainGeneration/TerrainGeneration.java
@@ -6,8 +6,10 @@ import java.lang.Math;
 public class TerrainGeneration {
 	public static void main(String[] args){
 		TerrainGeneration tg = new TerrainGeneration();
-		Color[][] colour = tg.drawMap(tg.generateMap(808, 10, 10));
-		System.out.println(colour[3][2]);
+		Color[][] image = tg.drawMap(tg.generateMap(808, 10, 10));
+		System.out.println(image[3][2]);
+		//option 1: png
+		//option 2: without png
 	}
 
 	public double[][] generateMap(long seed, int width, int height) {
@@ -17,19 +19,19 @@ public class TerrainGeneration {
 
 		for(int x=0; x<width; x++){
 			for(int y=0; y<height; y++){
-				map[x][y] = Math.abs(simplex.eval(x, y));
+				map[x][y] = Math.abs(simplex.eval(x, y)); //make maths scale from 0 to 1
 			}
 		}
 		return map;
 	}
 
 	public Color[][] drawMap(double[][] map) {
-		Color colour[][] = new Color[10][10];
+		Color image[][] = new Color[10][10];
 		for(int x = 0; x < map.length; x++) {
 			for(int y = 0; y < map[0].length; y++) {
-				colour[x][y] = new Color((float) map[x][y], (float) map[x][y], (float) map[x][y]);
+				image[x][y] = new Color((float) map[x][y], (float) map[x][y], (float) map[x][y]);
 			}
 		}
-		return colour;
+		return image;
 	}
 }
