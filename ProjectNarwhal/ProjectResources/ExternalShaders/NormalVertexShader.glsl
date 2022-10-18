6ed5b0675818d9932fbd30732ac02924bb248d75
Dereferenced-Null <lplace9001@gmail.com>
2022-10-18 14:23:09 +1100

    Fixed Normal Mapping
    Corrected Normal Vertex shader

--- ProjectNarwhal/ProjectResources/ExternalShaders/NormalVertexShader.glsl
+++ ProjectNarwhal/ProjectResources/ExternalShaders/NormalVertexShader.glsl
@@ -1,86 +1,71 @@
 #version 400 core
 
-in vec2 TextureCoords;
-in vec3 surfaceNormal;
-in vec3 toLightVector[10];
-in vec3 toCameraVector;
-in float visibility;
-in float NumberOfLights;
-in vec4 shadowCoords;
-in vec3 viewVector;
-in vec3 normalizedNormal;
+in vec3 position;
+in vec2 textureCoords;
+in vec3 normal;
+in vec3 tangent;
 
-uniform sampler2D textureSampler;
-uniform vec3 lightColor[10];
-uniform vec3 attenuation[10];
-uniform float shineDamper;
-uniform float reflectance;
-uniform vec3 waterColor;
-uniform sampler2D shadowMap;
-uniform samplerCube enviroMap;
-uniform float reflectivity;
-uniform sampler2D normalMap;
+out vec2 TextureCoords;
+out vec3 surfaceNormal;
+out vec3 toLightVector[10];
+out vec3 toCameraVector;
+out float visibility;
+out float NumberOfLights;
+out vec4 shadowCoords;
+out vec3 normalizedNormal;
+out vec3 viewVector;
 
-out vec4 outColor;
+uniform float numberOfLights;
+uniform mat4 transformationMatrix;
+uniform mat4 projectionMatrix;
+uniform mat4 viewMatrix;
+uniform vec3 lightPosition[10];
+uniform mat4 toShadowMapSpace;
+uniform float shadowDistance;
+uniform vec4 plane;
+uniform vec3 cameraPosition;
 
-const int pcfCount = 3;
-const float totalTexels = (pcfCount *2.0 + 1.0) * (pcfCount *2.0 + 1.0);
+const float density = 0.003;
+const float gradient = 1.5;
+const float transitionDistance = 10.0;
 
-void main(){
+void main()
+{
+    vec4 worldPosition = transformationMatrix * vec4(position,1.0);
+    shadowCoords = toShadowMapSpace * worldPosition;
+    vec4 positionRelativeToCam = viewMatrix * worldPosition;
+    mat4 modelViewMatrix = viewMatrix * transformationMatrix;
+    gl_Position = projectionMatrix * positionRelativeToCam;
+    TextureCoords = textureCoords;
 
-    vec4 normalMapValue = 2.0 * texture(normalMap, TextureCoords) - 1.0;
+    gl_ClipDistance[0] = dot(worldPosition, plane);
 
-    float mapSize = 4096.0;
-    float texelSize = 1.0/mapSize;
-    float total = 0.0;
+    surfaceNormal = (transformationMatrix * vec4(normal,0.0)).xyz;
+    vec3 norm = normalize(surfaceNormal);
+    vec3 tang = (modelViewMatrix * vec4(tangent, 0.0)).xyz;
+    vec3 unitTang = normalize(tang);
+    vec3 bitang = normalize(cross(norm, unitTang));
 
-    for(int x = -pcfCount; x <= pcfCount; x ++){
-        for(int y = -pcfCount; y <= pcfCount; y ++){
-            float objectNearestLight = texture(shadowMap, shadowCoords.xy + vec2(x,y) * texelSize).r;
-            if (shadowCoords.z > objectNearestLight + 0.002){
-                total += 1.0;
-            }
-        }
+    mat3 toTangentSpace = mat3 (
+        unitTang.x, bitang.x, norm.x,
+        unitTang.y, bitang.y, norm.y,
+        unitTang.z, bitang.z, norm.z
+    );
+
+    for(int i = 0; i < numberOfLights; i ++){
+        toLightVector[i] = toTangentSpace * (lightPosition[i] - worldPosition.xyz);
     }
-    total /= totalTexels;
-    float lightFactor = 1.0 - (total * shadowCoords.w);
 
-    vec3 unitNormal = normalize(normalMapValue.rgb);
-    vec3 totalDiffuse = vec3(0.0);
-    vec3 totalSpecular = vec3(0.0);
-    vec3 unitVectorToCamera = normalize(toCameraVector);
+    toCameraVector = toTangentSpace * ((inverse(viewMatrix) * vec4(0.0,0.0,0.0,1.0)).xyz - worldPosition.xyz);
+    viewVector = normalize(worldPosition.xyz - cameraPosition);
+    normalizedNormal = normalize(normal);
 
-    for(int i = 0 ; i < NumberOfLights; i++){
-        float distance = length(toLightVector[i]);
-        float attFactor = (attenuation[i].x) + (attenuation[i].y * distance) + (attenuation[i].z * distance * distance);
-        if(attFactor == 0){attFactor = 1;}
-        vec3 unitLightVector = normalize(toLightVector[i]);
-        float nDot1 = dot(unitNormal, unitLightVector);
-        float brightness = max(nDot1, 0.0);
-        vec3 lightDirection = -unitLightVector;
-        vec3 reflectedLightDirection = reflect(lightDirection, unitNormal);
-        float specularFactor = dot(reflectedLightDirection, unitVectorToCamera);
-        specularFactor = max(specularFactor,0.0);
-        float dampedFactor = pow(specularFactor,shineDamper);
-        if(i == 0){
-            totalDiffuse = totalDiffuse + ((brightness * lightColor[i]) / attFactor) * lightFactor;
-            totalSpecular = totalSpecular + ((dampedFactor * reflectance * lightColor[i]) / attFactor) * lightFactor;
-        }
-        else{
-            totalDiffuse = totalDiffuse + ((brightness * lightColor[i]) / attFactor);
-            totalSpecular = totalSpecular + ((dampedFactor * reflectance * lightColor[i]) / attFactor);
-        }
-    }
-    totalDiffuse = max(totalDiffuse, 0.1);
-    totalSpecular = totalSpecular;
+    float distance = length(positionRelativeToCam.xyz);
+    visibility = exp(-pow((distance*density),gradient));
+    visibility = clamp(visibility,0.0,1.0);
+    NumberOfLights = numberOfLights;
 
-    vec3 reflectedVector = reflect(viewVector, normalizedNormal);
-    vec3 refractedVector = refract(viewVector, normalizedNormal, 1.0/1.33);
-    vec4 reflectedColor = texture(enviroMap, reflectedVector);
-    vec4 refractedColor = texture(enviroMap, refractedVector);
-    vec4 enviroColor = mix(reflectedColor, refractedColor, 0.75);
-
-    outColor = vec4(totalDiffuse,1.0) * texture(textureSampler, TextureCoords) + vec4(totalSpecular, 1.0);
-    outColor = mix(outColor, enviroColor, reflectivity);
-    outColor = mix(vec4(waterColor, 1.0), outColor, visibility);
+    distance = distance - shadowDistance - transitionDistance;
+    distance = distance / transitionDistance;
+    shadowCoords.w = clamp(1.0-distance, 0.0,1.0);
 }
